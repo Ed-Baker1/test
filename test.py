138fe8ea065910f214634604a92fd7ea65c89f01
Ed-Baker1 <edward.baker@lowrisc.org>
2026-06-16 13:55:22 +0100

    added variables rabbit and string and print both

--- test.py
+++ test.py
@@ -1,7 +1,13 @@
 # A simple script to print text and numbers
+
 message = "The total count is"
 count = 42
 
 print(message, count)
 print("Hello World! 1 2 3 4 5 6 7 8 9 10")
 print("This is a test script for the test branch")
+
+string = "This is a string"
+rabbit = 8
+print(string)
+print(rabbit)   
